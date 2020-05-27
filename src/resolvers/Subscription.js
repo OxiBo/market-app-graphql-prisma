91517df15c4d8ad1a2e43aaa4e868e277c790793
OxiBo <xenos2000@ukr.net>
2020-05-27 17:04:59 -0400

    Add itemOrder subscription

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,3 +1,6 @@
+import userId from "../utils/getUserId";
+import getUserId from "../utils/getUserId";
+
 const Subscription = {
   product: {
     subscribe(parent, args, { prisma }, info) {
@@ -14,16 +17,59 @@ const Subscription = {
     },
   },
   review: {
-      subscribe(parent, args, {prisma }, info){
-          return prisma.subscription.review({
-              where: {
-                  node: {
-                      published: true
-                  }
-              }
-          }, info)
+    subscribe(parent, args, { prisma }, info) {
+      return prisma.subscription.review(
+        {
+          where: {
+            node: {
+              published: true,
+            },
+          },
+        },
+        info
+      );
+    },
+  },
+  orderItem: {
+    async subscribe(parent, args, { prisma, request }, info) {
+      const id = getUserId(request);
+      const userExists = await prisma.exists.User({
+        id,
+      });
+    //   console.log("user " + userExists)
+      const sellerExists = await prisma.exists.Seller({
+        id,
+      });
+    //   console.log("seller " + sellerExists)
+      if (!userExists && !sellerExists) {
+        throw new Error("You have to be logged in to see order items");
       }
-  }
+
+      const opArgs = {};
+      if (userExists) {
+        opArgs.where = {
+          node: {
+            user: {
+              id,
+            },
+          },
+        };
+      }
+      if (sellerExists) {
+        opArgs.where = {
+          node: {
+            product: {
+              seller: {
+                id,
+              },
+            },
+          },
+        };
+      }
+
+      return prisma.subscription.orderItem(opArgs, info);
+    },
+  },
 };
 
 export default Subscription;
